e413f446098fe00a5bc34cc019a2eb6834b53697
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-05 21:48:38 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int F = GetInt();
+int S = GetInt();
+
+int GetInt()
+{
+    System.Console.WriteLine("Введите целое число: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+int n = 0;
+int res = 0;
+
+for (n=1; n<=S; n++)
+{
+    res += F * F;
+}
+
+Console.WriteLine(res);
+
+
